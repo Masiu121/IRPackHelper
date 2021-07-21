a4b6fab4f0f7b6d745e3be3b5d623149dab5db4b
Masiu121 <maks.kapolka@gmail.com>
2021-07-22 00:34:26 +0200

    Fixed directory creating

--- src/com/Maksuu121/IRPackHelper/IRPackHelper.java
+++ src/com/Maksuu121/IRPackHelper/IRPackHelper.java
@@ -1,7 +1,5 @@
 package com.Maksuu121.IRPackHelper;
 
-import jdk.jfr.internal.tool.Main;
-
 import java.io.File;
 import java.util.Scanner;
 
@@ -30,7 +28,7 @@ public class IRPackHelper {
         System.out.println("2. Choose existing pack");
         System.out.println("3. Exit");
 
-        print("Choose number: ");
+        System.out.print("Choose number: ");
         int choice = scanner.nextInt();
 
         switch (choice) {
@@ -44,38 +42,31 @@ public class IRPackHelper {
                 exit = true;
                 break;
             default:
-                printLn("\nYou have to choose a number between 1 - 3");
+                System.out.println("\nYou have to choose a number between 1 - 3");
                 break;
         }
     }
 
     private static void newPack() {
-        print("Pack directory: ");
+        System.out.print("Pack directory: ");
+        scanner.nextLine();
         file = new File(scanner.nextLine());
         if(!file.isDirectory()) {
-            boolean dirCreated = file.mkdir();
-            if (dirCreated)
-                printLn("Directory successfully created!");
+            if (file.mkdirs())
+                System.out.println("Directory successfully created!");
             else
-                printLn("Can't create directory");
+                System.out.println("Can't create directory");
         }
     }
 
     private static void existingPack() {
-        print("Pack directory: ");
+        System.out.print("Pack directory: ");
+        scanner.nextLine();
         file = new File(scanner.nextLine());
 
         if(!file.isDirectory()) {
-            printLn("Directory does not exist, or it's invalid! (Pack has to be unpacked)");
+            System.out.println("Directory does not exist, or it's invalid! (Pack has to be unpacked)");
             file = null;
         }
-    }
-
-    private static void printLn(String message) {
-        System.out.println(message);
-    }
-
-    private static void print(String message) {
-        System.out.println(message);
     }
 }
